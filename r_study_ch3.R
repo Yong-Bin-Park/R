e1a719f3c2fe6ec744a9c537e762cc88ce5a7105
Yong-Bin-Park <90170614+Yong-Bin-Park@users.noreply.github.com>
2021-12-17 17:13:33 +0900

    Add files via upload

--- r_study_ch3.R
+++ r_study_ch3.R
@@ -6,6 +6,53 @@ if(x%%2==0) {
   print('x는 홀수')
 }
 
+blood <- 'B'
+if(blood == 'A'){
+  print("혈액형은 A형")
+} else if(blood == 'B'){
+  print("혈액형은 B형")
+} else if(blood == 'AB'){
+  print('혈액형은 AB형')
+} else {
+  print("혈액형은 O형")
+}
+
+# p.89
+x <- 10
+even <- ifelse(x%%2 == 0, 'x는 짝수', 'x는 홀수')
+print(even)
+
+# p.90
+for(i in 1:5){
+  print('x')
+}
+
+sum <- 0
+for(i in 1:10){
+  sum <- sum+i
+  print(sum)
+}
+
+# p. 92
+data <- mtcars
+class(data)
+ncol(data)
+colnames(data)
+c_name <- colnames(data)
+class(c_name)
+for( name in c_name){
+  print(name)
+}
+
+# p.94
+sum <- 0
+i <- 1
+while(i <= 10){
+  sum <- sum+i
+  print(sum)
+  i <- i+1
+}
+
 #3-16 두 수를 더하는 함수 add()의 작성과 실행
 #add 함수 작성
 add<-function(x,y){
